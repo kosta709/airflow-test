0aa19fab1851a152eef856b06c275ea1e62c5071
kosta709 <kosta777@gmail.com>
2022-03-10 17:19:26 +0200

    sigterm issue reproducing

---
+++ README.md
@@ -0,0 +1,6 @@
+# test airflow behaviour
+
+## Copy Test DAG to GCP Composer Bucket:
+COMPOSER_BUCKET=******
+gsutil -m rsync -rc ./dags gs://${COMPOSER_BUCKET}/dags
+
---
+++ dags/utils/utils.py
@@ -0,0 +1,33 @@
+from datetime import timedelta
+from datetime import datetime
+
+
+import os, time
+
+# from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+def log(message):
+    print(message)
+
+def do_something(task_id, duration_seconds):
+    #task_id = kwargs["task_id"][0], 
+    #   duration_seconds = kwargs["duration_seconds"][0],
+    log(f"Starting task_id={task_id} - duration_seconds={duration_seconds}")
+    time.sleep(duration_seconds)
+    log(f"Finish task_id={task_id} - duration_seconds={duration_seconds}")
+
+def long_task(task_name, duration_seconds, dag):
+    
+    #task_id = f"{task_name}-{duration_seconds}s"
+    task_id = f"{task_name}"
+    _task = PythonOperator(
+            task_id=task_id,
+            # provide_context=False,
+            python_callable=do_something,
+            op_kwargs={
+                'task_id': task_id,
+                'duration_seconds': duration_seconds
+            },
+            dag=dag)
+    return _task
